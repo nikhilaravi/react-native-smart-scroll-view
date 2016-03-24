7c63cdd27f107a65fb74553706270e58afc11a1b
Matt Labrum <matt@labrum.me>
2016-03-24 15:35:33 +1030

    Fix typo of keyboardWillShow

--- SmartScrollView.js
+++ SmartScrollView.js
@@ -46,7 +46,7 @@ class SmartScrollView extends Component {
       this._focusField('input_' + this.props.forceFocusField)
     }
     this._listeners = [
-      DeviceEventEmitter.addListener(Platform.OS == 'IOS' ? 'keyboardDidShow' : 'keyboardDidShow', this._keyboardWillShow),
+      DeviceEventEmitter.addListener(Platform.OS == 'IOS' ? 'keyboardWillShow' : 'keyboardDidShow', this._keyboardWillShow),
       DeviceEventEmitter.addListener(Platform.OS == 'IOS' ? 'keyboardWillHide' : 'keyboardDidHide', this._keyboardWillHide),
     ];
   }
