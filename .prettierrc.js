cb34ad0807c95431a63841d7a2b6d829b179915c
David Wheatley <hi@davwheat.dev>
2020-11-19 21:07:12 +0000

    Add Prettier for formatting

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 145,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  endOfLine: 'crlf',
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+};
